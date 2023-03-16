24ec84a4faa77ac27de7f444ff606f5cdeb74ba0
irwingcampanati <irwingcampanati.cube@gmail.com>
2023-03-16 16:58:02 -0300

    att

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Faça um programa em C# que imprima a soma dos números de 1 até 1000.
+
+using System;
+
+namespace SomaDeNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                soma = soma + i;
+            }
+            Console.WriteLine("A soma de números de 1 a 1000 é: " + soma);
+        }
+    }
+}
